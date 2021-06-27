4447782706c49435873106b12a1e978f11badfbc
MIDHUN M <83751603+MIDHUN-m2002@users.noreply.github.com>
2021-06-27 15:02:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>javascript and reactjs</h1>
+      <p>
+        this web page is created<b>by midhun m</b> as a part of a 7 days bootcamp with SHAPEAI
+        using react js framework
+        
+      </p>
+    </div>
+  );
+}
+
+export default Info;
